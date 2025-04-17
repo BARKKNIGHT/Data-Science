47d288fd53140f0d687a1d22be90dea437974c6e
BARKKNIGHT <146512484+BARKKNIGHT@users.noreply.github.com>
2025-04-17 15:28:22 +0530

    __

--- pow.py
+++
@@ -1,8 +0,0 @@
-def proof_of_work(block, difficulty):
-    nonce = 0
-    while True:
-        block.nonce = nonce
-        hash = calculate_hash(block)
-        if hash.startswith('0' * difficulty):
-            return nonce
-        nonce += 1
